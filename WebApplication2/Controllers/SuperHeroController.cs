3babc169b02fadede0fd884c8a1ccd38bcc32c5c
儒銘 簡 <diodio2222@hotmail.com>
2024-05-20 21:10:59 +0800

    建立分支和新方法

---
+++ WebApplication2/Controllers/SuperHeroController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public string aaaa() {
+            return "1";
+        }
+    }
+}
--- WebApplication2/Controllers/WeatherForecastController.cs
+++ WebApplication2/Controllers/WeatherForecastController.cs
@@ -19,7 +19,7 @@ namespace WebApplication2.Controllers
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<WeatherForecast> Insert()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
